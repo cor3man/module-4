4dfd79e69173a317aed2e0b44a8142e390ed14cf
Malena <Malena@DESKTOP-B0T6KCH>
2020-02-04 22:04:14 +0300

    Commit 1

---
+++ Raisa/Raisa/ClassB.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raisa
+{
+    class ClassB
+    {
+        private int b;
+        public static void MethodB() { Console.WriteLine("Class B"); }
+    }
+}
